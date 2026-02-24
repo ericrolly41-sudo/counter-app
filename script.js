2efb9fdde2029ad3764d2c2e723772820e165eb5
ericrolly41-sudo <ericrolly41@gmail.com>
2026-02-24 19:55:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let num=0
+
+function minus(){
+    num=num-1
+    document.getElementById("display").innerHTML=num
+}
+function clear1(){
+    num=0
+    document.getElementById("display").innerHTML=num
+}
+function add(){
+    num=num+1
+    document.getElementById("display").innerHTML=num
+}
